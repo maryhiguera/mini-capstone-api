a0a4c86388f1ffcf61bc3d3066fc21fdca7d9266
maryhiguera <marylin0higuera@gmail.com>
2025-07-07 19:21:24 -0600

    updated url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://mini-capstone.peterxjang.com", "http://localhost:5173", "mini-capstone-frontend"
+    origins "https://mini-capstone.peterxjang.com", "http://localhost:5173", "mini-capstone-frontend.netlify.app"
     resource "*",
              headers: :any,
              methods: %i[get post put patch delete options head],
