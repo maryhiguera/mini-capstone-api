0574f182b7b278973b3e58892331ff47fe1e3dae
maryhiguera <marylin0higuera@gmail.com>
2025-07-07 19:24:01 -0600

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,4 @@
-# # Create suppliers
-# supplier1 = Supplier.create(name: "Amazon", email: "amazon@email.com", phone_number: "243222342")
-# supplier2 = Supplier.create(name: "Think Geek", email: "thinkgeek@email.com", phone_number: "5432534")
-
-# # Create products and their associated images
-# product1 = Product.create({
-#   supplier_id: supplier2.id,
-#   name: "Lightsaber",
-#   price: 270.10,
-#   description: "Part laser, part samurai sword, all awesome. The lightsaber is an elegant weapon for a more civilized age, not nearly as clumsy as a blaster"
-# })
-# Image.create(url: "http://25.media.tumblr.com/d2456964024018fd930338c099371104/tumblr_n2m73lTx2Q1svn82uo1_400.gif", product_id: product1.id)
-
-# product2 = Product.create({
-#   supplier_id: supplier1.id,
-#   name: "Space Cowboy Laser Gun",
-#   price: 170.0,
-#   description: "This weapon has no other description than, Shiney!"
-# })
-# Image.create(url: "http://cdn.shopify.com/s/files/1/0289/1534/products/MalPistol_MP-1_1756x988_e53f9448-81ec-41de-9369-4cbad64f18f5_1024x1024.jpg?v=1401915776", product_id: product2.id)
-
-# product3 = Product.create({
-#   supplier_id: supplier2.id,
-#   name: "DnD Dice set",
-#   price: 57.50,
-#   description: "Take down mighty dragons with this timeless set of 20 sided wonders"
-# })
-# Image.create(url: "https://s-media-cache-ak0.pinimg.com/736x/9c/15/7b/9c157bea5331463f9c539cbce739a4b8.jpg", product_id: product3.id)
-
-# product4 = Product.create({
-#   supplier_id: supplier1.id,
-#   name: "Sonic Screwdriver",
-#   price: 9.99,
-#   description: "The Doctor's sciencey magic wand to get out of tight spots. Note: does not work on wood"
-# })
-# Image.create(url: "https://sketchfab.com/blogs/community/wp-content/uploads/2020/04/image2-2.jpg", product_id: product4.id)
-
-# product5 = Product.create({
-#   supplier_id: supplier2.id,
-#   name: "Yoda sleeping bag",
-#   price: 40.00,
-#   description: "For real"
-# })
-# Image.create(url: "https://staticdelivery.nexusmods.com/mods/1151/images/12353-0-1461721930.png", product_id: product5.id)
+# db/seeds.rb
 # ------------------------------------------------------------------
 # Populates:
 #   • 1 admin + 10 regular users
@@ -61,7 +18,7 @@ require "bigdecimal/util"
 TAX_RATE = 0.05 # 5 %
 
 puts "\n🌱  Resetting tables…"
-[ Image, CategoryProduct, Category, Order,
+[ Image, CategoryProduct, Category, CartedProduct, Order,
  Product, Supplier, User ].each(&:delete_all)
 
 # ---------------------------- Users -------------------------------
@@ -132,30 +89,50 @@ puts "🧾  Creating orders…"
 users = User.where(admin: false)
 
 100.times do
-  product  = products.sample
-  quantity = rand(1..5)
-  subtotal = (product.price * quantity).to_d
-  tax      = (subtotal * TAX_RATE).round(2)
-  total    = subtotal + tax
+  user = users.sample
 
-  Order.create!(
-    user:     users.sample,
-    product:  product,
-    quantity: quantity,
+  # Create 1-3 carted products for this order
+  carted_products = []
+  rand(1..3).times do
+    product = products.sample
+    quantity = rand(1..5)
+
+    carted_products << CartedProduct.create!(
+      user: user,
+      product: product,
+      quantity: quantity,
+      status: "purchased"
+    )
+  end
+
+  # Calculate totals for the order
+  subtotal = carted_products.sum { |cp| (cp.product.price * cp.quantity).to_d }
+  tax = (subtotal * TAX_RATE).round(2)
+  total = subtotal + tax
+
+  # Create the order
+  order = Order.create!(
+    user: user,
     subtotal: subtotal,
-    tax:      tax,
-    total:    total
+    tax: tax,
+    total: total
   )
+
+  # Associate the carted products with the order
+  carted_products.each do |cp|
+    cp.update!(order: order)
+  end
 end
 
 # --------------------------- Summary ------------------------------
 puts "\n✅  Seeding complete!"
-puts "   Users:      #{User.count} (#{User.where(admin: true).count} admin)"
-puts "   Suppliers:  #{Supplier.count}"
-puts "   Categories: #{Category.count}"
-puts "   Products:   #{Product.count}"
-puts "   Images:     #{Image.count}"
-puts "   Orders:     #{Order.count}"
+puts "   Users:         #{User.count} (#{User.where(admin: true).count} admin)"
+puts "   Suppliers:     #{Supplier.count}"
+puts "   Categories:    #{Category.count}"
+puts "   Products:      #{Product.count}"
+puts "   Images:        #{Image.count}"
+puts "   Orders:        #{Order.count}"
+puts "   Cart Items:    #{CartedProduct.count}"
 
 # If you ever need to reuse Faker's unique generators elsewhere:
 #   Faker::UniqueGenerator.clear
